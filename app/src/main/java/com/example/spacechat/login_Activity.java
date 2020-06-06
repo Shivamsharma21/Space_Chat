4d22a2520af009da00addce5d7d9f5713e07ac2d
Shivamsharma21 <intelhybrid21@gmail.com>
2020-06-06 17:24:44 +0530

    1 Views Are Initialized in register and Login Activity. 2 FireBase Create Account functionality added. 3 ProgressDialog is Added. 4  Improvements in layout. 5 Bugs fixes(Firebase).

--- app/src/main/java/com/example/spacechat/login_Activity.java
+++ app/src/main/java/com/example/spacechat/login_Activity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class login_Activity extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
+
+     private Button Login_Button,Login_Phone_Btn;
+        private TextView forgetpassword,newuser_register;
+         private EditText login_Email,login_password;
+
+
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -27,5 +38,24 @@ public class login_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_);
+      Initilizer();
+         newuser_register.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent registerActivityintent = new Intent(login_Activity.this,register_Activity.class);
+                 startActivity(registerActivityintent);
+             }
+         });
     }
+
+    private void Initilizer() {
+       Login_Button = findViewById(R.id.login_btn);
+       Login_Phone_Btn = findViewById(R.id.Using_phone_Button);
+        forgetpassword = findViewById(R.id.forget_password);
+         newuser_register = findViewById(R.id.Register_New_Account);
+          login_Email = findViewById(R.id.login_email);
+          login_password = findViewById(R.id.login_password);
+    }
+
+
 }
--- app/src/main/java/com/example/spacechat/register_Activity.java
+++ app/src/main/java/com/example/spacechat/register_Activity.java
@@ -1,14 +1,83 @@
 package com.example.spacechat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class register_Activity extends AppCompatActivity {
+     private FirebaseAuth firebaseAuth;
+     private Button register_button;
+     private EditText Register_Email,Register_Password;
+     private TextView Already_Have_Account;
+     private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_);
+        Initilizer();
+
+    Already_Have_Account.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent Already_have_Account_intent = new Intent(register_Activity.this,login_Activity.class);
+            startActivity(Already_have_Account_intent);
+        }
+    });
+        register_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreateNewAccount();
+            }
+        });
     }
+
+    private void CreateNewAccount(){
+          progressDialog.setTitle("Creating a new account");
+          progressDialog.setMessage("Please Wait");
+          progressDialog.show();
+             String email = Register_Email.getText().toString();
+             String password = Register_Password.getText().toString();
+        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+               if(task.isSuccessful()){
+                   progressDialog.dismiss();
+                   Toast.makeText(register_Activity.this, "Account is Created", Toast.LENGTH_SHORT).show();
+                    SendUserToLoginActivity();
+               }else{
+                   progressDialog.dismiss();
+                   String message = task.getException().toString();
+                   Toast.makeText(register_Activity.this, "Error "+message, Toast.LENGTH_SHORT).show();
+               }
+            }
+        });
+
+    }
+    private void Initilizer() {
+      register_button = findViewById(R.id.signup_btn);
+      Register_Email = findViewById(R.id.signup_email);
+      Register_Password = findViewById(R.id.signup_password);
+      Already_Have_Account = findViewById(R.id.already_have_account);
+      firebaseAuth = FirebaseAuth.getInstance();
+      progressDialog = new ProgressDialog(this);
+    }
+      private void SendUserToLoginActivity(){
+        Intent loginintent = new Intent(register_Activity.this,login_Activity.class);
+         startActivity(loginintent);
+      }
 }
